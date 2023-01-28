745c5d3f4d78c2c6e5ce999416a9ac4bf70c684f
Joel Singh <joelsingh788@gmail.com>
2023-01-28 06:41:35 -0500

    Add location data to characterPicker

--- src/components/CharacterPicker.js
+++ src/components/CharacterPicker.js
@@ -1,10 +1,12 @@
 function CharacterPicker(props) {
-  const { isVisible } = props;
+  const { isVisible, location = { x: 0, y: 0 } } = props;
 
-  const visibleClass = isVisible ? ' visible' : '';
+  const visibleClass = isVisible ? " visible" : "";
   return (
     <div
       className={"character-picker" + visibleClass}
+      data-x={location.x}
+      data-y={location.y}
     >
       Hello World
     </div>
