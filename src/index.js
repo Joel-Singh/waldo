1475a831584fa6fb9a6e514ce40abf6d1ee55afc
Joel Singh <joelsingh788@gmail.com>
2023-01-30 14:09:35 -0500

    Remove css import in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import { BrowserRouter } from "react-router-dom";
-import css from './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
