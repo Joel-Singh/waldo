c689db3def646201941a80ed6cafb9347868f57a
Joel Singh <joelsingh788@gmail.com>
2023-02-02 10:17:50 -0500

    Add firebase code

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { getDatabase } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
+export default function initializeFirebase() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBi3Vk9CTvjIUA3SzS94tlSpxwVKd38qWM",
+    authDomain: "waldo-a317d.firebaseapp.com",
+    projectId: "waldo-a317d",
+    storageBucket: "waldo-a317d.appspot.com",
+    messagingSenderId: "356577803449",
+    appId: "1:356577803449:web:9e7be0752c53b8aab42f68",
+    databaseURL: "https://waldo-a317d-default-rtdb.firebaseio.com/",
+  };
+
+  const app = initializeApp(firebaseConfig);;
+  const database = getDatabase(app);
+}
