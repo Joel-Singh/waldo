9741d0c1a5793ce23c9b501643e2d3dab9b59288
Joel Singh <joelsingh788@gmail.com>
2023-02-06 15:56:45 -0500

    Set up routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Gamescreen from "./components/Gamescreen";
+import SelectionScreen from "./components/SelectionScreen";
+
+const mazeGamescreen = <Gamescreen />;
+const beachGamescreen = <Gamescreen />;
+const snowGamescreen = <Gamescreen />;
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Routes>
+        <Route path="*" element={null}/>
+        {/* Add map previews to this selection screen */}
+        <Route path="/" element={<SelectionScreen />}/>
+      </ Routes>
+      <Routes>
+        <Route path="/maze" element={mazeGamescreen}/>
+        <Route path="/beach" element={beachGamescreen}/>
+        <Route path="/snow" element={snowGamescreen}/>
+      </ Routes>
     </div>
   );
 }
