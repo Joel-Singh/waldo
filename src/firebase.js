ba1ae424363e287469abf727c72da70f0659a8c6
Joel Singh <joelsingh788@gmail.com>
2023-02-06 08:03:38 -0500

    Add addCharacterCoordsToDatabase function

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set, connectDatabaseEmulator } from "firebase/database";
 import { initializeApp } from "firebase/app";
 
+let db;
 export default function initializeFirebase() {
   const firebaseConfig = {
     apiKey: "AIzaSyBi3Vk9CTvjIUA3SzS94tlSpxwVKd38qWM",
@@ -12,9 +13,43 @@ export default function initializeFirebase() {
     databaseURL: "https://waldo-a317d-default-rtdb.firebaseio.com/",
   };
 
-  const app = initializeApp(firebaseConfig);;
-  const database = getDatabase(app);
-  if (location.hostname === "localhost") {
-    connectDatabaseEmulator(db, "localhost", 9000);
+  const app = initializeApp(firebaseConfig);
+  db = getDatabase(app);
+  connectDatabaseEmulator(db, "localhost", 9000);
+}
+
+export function testDatabaseSetting() {
+  set(ref(db, 'users/37'), {
+    username: 'Joel',
+    email: 'joelsingh788@gmail.com',
+    profile_picture: 'penisman',
+  })
+}
+
+//TODO: Final app won't have this function
+//The character coords will already be in the database
+export function addCharacterCoordsToDatabase() {
+  const characterCoords = [
+    {
+      name: "name1",
+      coords: {x: 28, y: 10}
+    },
+    {
+      name: "name2",
+      coords: {x: 86, y: 30}
+    },
+    {
+      name: "name3",
+      coords: {x: 21, y: 90}
+    },
+  ]
+
+  characterCoords.forEach(({name, coords}) => {
+    addSingleCharacterCoordToDatabase(name, coords)
+  })
+
+  function addSingleCharacterCoordToDatabase(name, coords) {
+    set(ref(db, `characterCoordinates/${name}`), coords)
+      .catch(() => console.error("Couldn't add single character coordinate"))
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import { BrowserRouter } from "react-router-dom";
-import initializeFirebase from "./firebase.js"
+import initializeFirebase, { addCharacterCoordsToDatabase } from "./firebase.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +14,4 @@ root.render(
 );
 
 initializeFirebase();
+addCharacterCoordsToDatabase();
