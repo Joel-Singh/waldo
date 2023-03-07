c46ed65a9198137745ef3ee310c6e303a6e8970b
Joel Singh <joelsingh788@gmail.com>
2023-03-07 09:20:51 -0500

    Move CursorOverlay test with gamescreen into its own file

---
+++ src/components/__tests__/GamescreenInteractions/CursorOverlay.test.js
@@ -0,0 +1,20 @@
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { toHaveClass } from "@testing-library/jest-dom";
+import Gamescreen from "../../Gamescreen.js";
+
+test("Cursor overlay is hidden when character picker is visible", () => {
+  render(<Gamescreen />);
+
+  const gamescreen = screen.getByTestId("gamescreen");
+  const characterPicker = screen.getByTestId("character-picker");
+  const cursorOverlay = screen.getByTestId("cursor-overlay");
+
+  userEvent.click(gamescreen);
+  expect(characterPicker).toHaveClass("character-picker--visible");
+  expect(cursorOverlay).not.toHaveClass("cursor-overlay--visible");
+
+  userEvent.click(gamescreen);
+  expect(characterPicker).not.toHaveClass("character-picker--visible");
+  expect(cursorOverlay).toHaveClass("cursor-overlay--visible");
+});
