b639f1a08c28188327945254581592533d98fce4
Joel Singh <joelsingh788@gmail.com>
2023-03-07 09:47:07 -0500

    Fix capitalization

--- src/components/__tests__/Stopwatch.test.js
+++ src/components/__tests__/Stopwatch.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { decisecondToMs } from "../../util/constants";
 import Stopwatch from "../Stopwatch";
 
-it("Renders seconds elapsed", () => {
+it("renders seconds elapsed", () => {
   const secondsElapsed = 32.5;
   render(
     <Stopwatch
