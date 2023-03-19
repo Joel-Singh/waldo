40a39afe3727093eb32182bcb699558253a85376
Joel Singh <joelsingh788@gmail.com>
2023-03-19 15:37:59 -0400

    Format

--- src/components/__tests__/HighscoreScreen.test.js
+++ src/components/__tests__/HighscoreScreen.test.js
@@ -1,8 +1,10 @@
-import { render } from "@testing-library/react"
-import HighscoreScreen from "../HighscoreScreen"
+import { render } from "@testing-library/react";
+import HighscoreScreen from "../HighscoreScreen";
 
 test("Highscore screen renders", () => {
-  const { container: { firstChild : highscoreScreen} } = render(<HighscoreScreen />)
+  const {
+    container: { firstChild: highscoreScreen },
+  } = render(<HighscoreScreen />);
 
-  expect(highscoreScreen).toBeDefined()
-})
+  expect(highscoreScreen).toBeDefined();
+});
