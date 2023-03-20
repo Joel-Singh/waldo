454c8b01fb20645ab7807c0920c43165d867895b
Joel Singh <joelsingh788@gmail.com>
2023-03-20 15:05:12 -0400

    Format

--- src/components/HighscoreScreen.js
+++ src/components/HighscoreScreen.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import PropTypes from "prop-types"
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import getFirebaseFunctions from "../util/firebase";
 
 export default function HighscoreScreen({ map }) {
@@ -7,31 +7,31 @@ export default function HighscoreScreen({ map }) {
 
   useEffect(() => {
     setHighscores([]);
-    const { getTopTenHighscores } = getFirebaseFunctions()
+    const { getTopTenHighscores } = getFirebaseFunctions();
 
     let ignore = false;
 
-    getTopTenHighscores(map)
-      .then(topTenHighscores => {
-        if (!ignore)
-          setHighscores(topTenHighscores)
-      })
+    getTopTenHighscores(map).then((topTenHighscores) => {
+      if (!ignore) setHighscores(topTenHighscores);
+    });
 
     return () => {
       ignore = true;
     };
-  }, [map])
+  }, [map]);
 
   return (
-    <div data-testid="HighscoreScreen">{highscores.map(({initials, timeTaken}) =>
-      (<div key={initials}>
-        <span>{initials}</span>
-        <span>{timeTaken}</span>
-      </div>)
-    )}</div>
-  )
+    <div data-testid="HighscoreScreen">
+      {highscores.map(({ initials, timeTaken }) => (
+        <div key={initials}>
+          <span>{initials}</span>
+          <span>{timeTaken}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 HighscoreScreen.propTypes = {
-  map: PropTypes.string.isRequired
-}
+  map: PropTypes.string.isRequired,
+};
