0c771909b906a511acf6d6740a33369dfc76bf24
Joel Singh <joelsingh788@gmail.com>
2023-03-20 14:16:13 -0400

    Highscore screen renders scores

--- src/components/HighscoreScreen.js
+++ src/components/HighscoreScreen.js
@@ -1,3 +1,37 @@
-export default function HighscoreScreen() {
-  return (<div></div>)
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import getFirebaseFunctions from "../util/firebase";
+
+export default function HighscoreScreen({ map }) {
+  const [highscores, setHighscores] = useState([]);
+
+  useEffect(() => {
+    setHighscores([]);
+    const { getTopTenHighscores } = getFirebaseFunctions()
+
+    let ignore = false;
+
+    getTopTenHighscores(map)
+      .then(topTenHighscores => {
+        if (!ignore)
+          setHighscores(topTenHighscores)
+      })
+
+    return () => {
+      ignore = true;
+    };
+  }, [map])
+
+  return (
+    <div data-testid="HighscoreScreen">{highscores.map(({initials, timeTaken}) =>
+      (<div key={initials}>
+        <span>{initials}</span>
+        <span>{timeTaken}</span>
+      </div>)
+    )}</div>
+  )
+}
+
+HighscoreScreen.propTypes = {
+  map: PropTypes.string.isRequired
 }
--- src/components/__tests__/HighscoreScreen.test.js
+++ src/components/__tests__/HighscoreScreen.test.js
@@ -1,10 +1,48 @@
-import { render } from "@testing-library/react";
+import { act, getByTestId, render, screen, waitFor } from "@testing-library/react";
+import getFirebaseFunctions from "../../util/firebase";
 import HighscoreScreen from "../HighscoreScreen";
 
-test("Highscore screen renders", () => {
-  const {
-    container: { firstChild: highscoreScreen },
-  } = render(<HighscoreScreen />);
+test("Highscore screen renders", async () => {
 
-  expect(highscoreScreen).toBeDefined();
+  render(<HighscoreScreen map="maze" />);
+
+  await act(async () => {
+    await new Promise(resolve => setTimeout(resolve, 100))
+  })
+
+  expect(screen.getByTestId("HighscoreScreen")).toBeDefined();
+});
+
+test("Highscore screen renders highscores", async () => {
+  const { addHighscore, clearHighscores } = getFirebaseFunctions();
+
+
+  const createHighscoreEntry = (initials, score) => ({ initials, score });
+  const initialsAndScores = [
+    createHighscoreEntry("AB", 1),
+    createHighscoreEntry("CD", 2),
+    createHighscoreEntry("EF", 3),
+    createHighscoreEntry("GH", 4),
+    createHighscoreEntry("IJ", 5),
+    createHighscoreEntry("KL", 6),
+    createHighscoreEntry("MN", 7),
+    createHighscoreEntry("OP", 8),
+    createHighscoreEntry("QR", 9),
+    createHighscoreEntry("ST", 10),
+  ];
+
+  await clearHighscores()
+  await Promise.all(
+    initialsAndScores.map(({ initials, score }) =>
+      addHighscore("maze", initials, score)
+    )
+  );
+
+  render(<HighscoreScreen map="maze" />);
+
+  await act(async () => {
+    await new Promise(resolve => setTimeout(resolve, 100))
+  })
+
+  expect(screen.getByTestId("HighscoreScreen")).toMatchSnapshot()
 });
