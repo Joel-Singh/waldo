20664ef0e44db69badda751a22a1da5f721e2978
Joel Singh <joelsingh788@gmail.com>
2023-03-23 09:27:10 -0400

    Add test for initially showing gamescreen and hiding highscore screen

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,17 @@ jest.mock('./components/HighscoreScreen.js', () => () => {
   return <div data-testid="HighscoreScreen"></div>;
 })
 
+it("initially shows gamescreen and initially hides highscore screen for maze", () => {
+  render(
+    <MemoryRouter initialEntries={['/maze']}>
+      <App />
+    </MemoryRouter>
+  )
+
+  expect(screen.getByTestId('gamescreen')).toBeDefined()
+  expect(screen.queryByTestId('HighscoreScreen')).toBeNull()
+})
+
 test("Shows highscore screen and hides gamescreen when all characters are chosen for maze", async () => {
   render(
     <MemoryRouter initialEntries={['/maze']}>
