1aa8c35a7d67b4d7a1c61ebdd43dabb668848f84
Joel Singh <joelsingh788@gmail.com>
2023-09-25 02:10:38 -0400

    Format

--- src/components/__tests__/FinalScoreScreen.test.tsx
+++ src/components/__tests__/FinalScoreScreen.test.tsx
@@ -2,6 +2,6 @@ import { render } from "@testing-library/react";
 import FinalScoreScreen from "../FinalScoreScreen";
 
 test("finalScoreScreen display passed in final score", () => {
-    const { container } = render(<FinalScoreScreen finalScore={32} />);
-    expect(container.outerHTML).toContain("32");
-  })
+  const { container } = render(<FinalScoreScreen finalScore={32} />);
+  expect(container.outerHTML).toContain("32");
+})
