47a86713647ad618357eb2c7c8436ed5b78e2b6b
Joel Singh <joelsingh788@gmail.com>
2023-09-26 01:20:03 -0400

    Format

--- src/components/FinalScoreScreen.tsx
+++ src/components/FinalScoreScreen.tsx
@@ -2,7 +2,7 @@ export default function FinalScoreScreen(props: { finalScore: number }) {
   const { finalScore } = props;
   return (
     <div data-testid="FinalScoreScreen">
-      <h1>Final Score</ h1>
+      <h1>Final Score</h1>
       <p>{finalScore} seconds!</p>
     </div>
   );
