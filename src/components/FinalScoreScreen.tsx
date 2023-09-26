858827c6d77a0b8a8bcf5d732cf2e68d96fbd3be
Joel Singh <joelsingh788@gmail.com>
2023-09-26 01:20:34 -0400

    Add back to selection screen button

--- src/components/FinalScoreScreen.tsx
+++ src/components/FinalScoreScreen.tsx
@@ -1,9 +1,20 @@
-export default function FinalScoreScreen(props: { finalScore: number }) {
-  const { finalScore } = props;
+import { MouseEventHandler } from "react";
+
+export default function FinalScoreScreen(props: {
+  finalScore: number;
+  backToSelectionScreenBtnOnClick?: MouseEventHandler<HTMLElement>;
+}) {
+  const {
+    finalScore,
+    backToSelectionScreenBtnOnClick = (e) => {}
+  } = props;
   return (
     <div data-testid="FinalScoreScreen">
       <h1>Final Score</h1>
       <p>{finalScore} seconds!</p>
+      <button onClick={backToSelectionScreenBtnOnClick}>
+        Go back to selection screen
+      </button>
     </div>
   );
 }
--- src/components/__tests__/FinalScoreScreen.test.tsx
+++ src/components/__tests__/FinalScoreScreen.test.tsx
@@ -1,7 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import FinalScoreScreen from "../FinalScoreScreen";
 
 test("finalScoreScreen display passed in final score", () => {
   const { container } = render(<FinalScoreScreen finalScore={32} />);
   expect(container.outerHTML).toContain("32");
+})
+
+test("backToSelectionScreenBtnOnClick properly passed into button", () => {
+  const mockFunc = jest.fn();
+  const { getByText } = render(<FinalScoreScreen finalScore={32} backToSelectionScreenBtnOnClick={mockFunc} />);
+  const backToSelectionScreenBtn = getByText("Go back to selection screen");
+  userEvent.click(backToSelectionScreenBtn);
+  expect(mockFunc).toBeCalled();
 })
