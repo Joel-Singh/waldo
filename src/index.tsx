eaf1a84c2a0ee22489888c094d7bcb084a4b5675
Joel Singh <joelsingh788@gmail.com>
2023-10-10 22:36:09 -0400

    Convert index to ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { exposeCharacterToleranceAsCssVariable } from "./util/constants";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 );
 
 exposeCharacterToleranceAsCssVariable();
